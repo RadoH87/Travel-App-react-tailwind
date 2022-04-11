5797b223a321efc0292f7232a7902a9f6b899fe7
Radek Holubowski <radekh8787@gmail.com>
2022-04-11 15:11:56 +0100

    Add:Destinations component & styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Navbar } from "./components/Navbar";
 import { Hero } from "./components/Hero";
+import { Destinantions } from "./components/Destinantions";
 
 export const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Destinantions />
     </div>
   );
 };
