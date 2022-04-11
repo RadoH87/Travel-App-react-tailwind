e9dc154e25496af5bb469db8bfd8e425f430a17c
Radek Holubowski <radekh8787@gmail.com>
2022-04-11 16:59:45 +0100

    Add: SelectsCard component & styling

--- src/components/Selects.jsx
+++ src/components/Selects.jsx
@@ -5,12 +5,17 @@ import Maldives from "../assets/maldives.jpg";
 import Maldives2 from "../assets/maldives2.jpg";
 import Maldives3 from "../assets/maldives3.jpg";
 import KeyWest from "../assets/keywest.jpg";
-import SelectsCard from "./SelectsCard";
+import { SelectsCard } from "./SelectsCard";
 
 export const Selects = () => {
   return (
     <div className="max-w-[1240px] mx-auto px-4 py-16 grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
-      <h1>selects</h1>
+      <SelectsCard bg={BoraBora} text="Bora Bora" />
+      <SelectsCard bg={BoraBora2} text="Maldives" />
+      <SelectsCard bg={Maldives} text="Antigua" />
+      <SelectsCard bg={Maldives2} text="Cozumel" />
+      <SelectsCard bg={Maldives3} text="Jamaica" />
+      <SelectsCard bg={KeyWest} text="Key West" />
     </div>
   );
 };
