dd05489186999c78f8efd416c7441187fb875726
Radek Holubowski <radekh8787@gmail.com>
2022-04-11 17:22:54 +0100

    Add:Carousel component with silderData & styling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Hero } from "./components/Hero";
 import { Destinantions } from "./components/Destinantions";
 import { Search } from "./components/Search";
 import { Selects } from "./components/Selects";
+import { Carousel } from "./components/Carousel";
 
 export const App = () => {
   return (
@@ -12,6 +13,7 @@ export const App = () => {
       <Destinantions />
       <Search />
       <Selects />
+      <Carousel />
     </div>
   );
 };
